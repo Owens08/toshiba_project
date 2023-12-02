2e9eabe9b05e0d41550659262f5ece72b5a31963
Owens08 <owenkaranja888@gmail.com>
2023-12-02 13:04:25 +0300

    Commit 6: Conversations and Details

--- woody/woody/settings.py
+++ woody/woody/settings.py
@@ -42,8 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'conversation',
     'dashboard',
     'item',
+
 ]
 
 MIDDLEWARE = [
--- woody/woody/urls.py
+++ woody/woody/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('items/', include('item.urls')),
+    path('inbox/', include('conversation.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('about/', about , name='about'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
